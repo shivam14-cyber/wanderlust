77e966862283f93a0ac1e1fe6d95eb75baf92a18
shivam <sk2650shivam@gmail.com>
2026-05-19 01:10:11 +0530

    adding the supportpart

--- app.js
+++ app.js
@@ -79,6 +79,43 @@ app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
 
+app.get("/artical/privacy",(req,res)=>{
+  res.render("privacy/privacy.ejs");
+});
+
+app.get("/artical/terms",(req,res)=>{
+  res.render("privacy/termas.ejs");
+});
+
+app.get("/artical/help",(req,res)=>{
+ res.render("artical/HelpCenter.ejs");
+});
+app.get("/artical/gethelp",(req,res)=>{
+ res.render("artical/Gethelp.ejs");
+})
+app.get("/artical/aircover",(req,res)=>{
+ res.render("artical/Aircover.ejs");
+})
+
+app.get("/artical/Anti",(req,res)=>{
+ res.render("artical/Anti.ejs");
+})
+
+app.get("/artical/disbility",(req,res)=>{
+ res.render("artical/disbility.ejs");
+})
+app.get("/artical/cancle",(req,res)=>{
+ res.render("artical/Cancle.ejs");
+})
+
+app.get("/artical/repoter",(req,res)=>{
+ res.render("artical/Repoter.ejs");
+})
+
+
+
+
+
 app.use((req, res) => {
 
   req.flash("error", "Page not found");
